246f5bda53fe80c174e55ff2d7e1678a858ffc1a
younghwa <xxnewnewxx0127@gmail.com>
2024-07-12 02:41:34 +0900

    noona api five

---
+++ main.js
@@ -0,0 +1,122 @@
+const API_KEY = `29a96ef9b757480189bbd02b34691b57`;
+let articles = [];
+let page = 1;
+let totalPage = 1;
+const pageSize = 8;
+let url = new URL(
+  `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${pageSize} `
+);
+let menus = document.querySelectorAll("#menu-list button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (e) => getNewsByTopic(e))
+);
+
+const getNews = async () => {
+  try {
+    url.searchParams.set("page", page);
+    console.log("Rrr", url);
+    let response = await fetch(url);
+    let data = await response.json();
+    if (response.status == 200) {
+      console.log("result", data);
+      if (data.totalResults == 0) {
+        page = 0;
+        totalPage = 0;
+        renderPagination();
+        throw new Error("일치하는 결과가 없습니다");
+      }
+
+      articles = data.articles;
+      totalPage = Math.ceil(data.totalResults / pageSize);
+      render();
+      renderPagination();
+    } else {
+      page = 0;
+      totalPage = 0;
+      renderPagination();
+      throw new Error(data.message);
+    }
+  } catch (e) {
+    errorRender(e.message);
+    page = 0;
+    totalPage = 0;
+    renderPagination();
+  }
+};
+
+////////////////  뉴스 카테고리 /////////////////////////////////////
+const getLatestNews = () => {
+  page = 1;
+  url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${pageSize}`
+  );
+  getNews();
+};
+
+const getNewsByTopic = (event) => {
+  const topic = event.target.textContent.toLowerCase();
+
+  page = 1;
+  url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${pageSize}&category=${topic}`
+  );
+  getNews();
+};
+
+/////////   검색어로 조회하기 /////////////////
+const getNewsByKeyword = () => {
+  const keyword = document.getElementById("search-input").value;
+
+  page = 1;
+  url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=${pageSize}&q=${keyword}`
+  );
+  getNews();
+};
+
+document.getElementById("search-input").addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    getNewsByKeyword();
+  }
+});
+
+const render = () => {
+  let resultHTML = articles
+    .map((news) => {
+      return `<div class="news row">
+        <div class="col-lg-4">
+            <img class="news-img"
+                src="${news.urlToImage}" 
+                onerror="this.src='https://media.istockphoto.com/id/1399859917/pt/vetorial/no-image-vector-symbol-missing-available-icon-no-gallery-for-this-moment-placeholder.jpg?s=612x612&w=0&k=20&c=sLIG7ZNWBtvCM1o_Po0X9Y3UUu0KajdLvrKlrsLlrk8='; this.onerror=null;" />
+        </div>
+        <div class="col-lg-8">
+            <a class="title" target="_blank" href="${news.url}">${
+        news.title
+      }</a>
+            <p>${
+              news.description == null || news.description == ""
+                ? "내용이 없습니다"
+                : news.description.length > 200
+                ? news.description.substring(0, 200) + "..."
+                : news.description
+            }</p>
+            <div>${news.source.name || "no source"}  ${moment(
+        news.publishedAt
+      ).fromNow()}</div>
+        </div>
+    </div>`;
+    })
+    .join("");
+
+  document.getElementById("news-board").innerHTML = resultHTML;
+};
+
+const openNav = () => {
+  document.getElementById("noonaSidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("noonaSidenav").style.width = "0";
+};
+
+getLatestNews();
